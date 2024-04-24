02284dee3dadff1595fe19c4cf315db01ad93106
singuyenvan0206 <96609459+singuyenvan0206@users.noreply.github.com>
2024-04-24 09:13:11 +0700

    Update NextDateTest.java

--- NextDate/src/test/java/NextDateTest.java
+++ NextDate/src/test/java/NextDateTest.java
@@ -10,12 +10,12 @@ public class NextDateTest {
         // Test case: 1/1/2020 -> 2/1/2020
         assertEquals("Invalid date", nextDate.getNextDate(1, 1, 2020));
 
-        // Test case: 31/12/2020 -> 1/1/2021
+        // Test case: 31/12/2012 -> 1/1/2021
         assertEquals("21/12/2012", nextDate.getNextDate(20, 12, 2012));
 
-        // Test case: 28/2/2020 (leap year) -> 29/2/2020
+        // Test case: 28/2/2012 (leap year) -> 29/2/2020
         assertEquals("29/2/2012", nextDate.getNextDate(28, 2, 2012));
 
-
+        assertEquals("1/3/2010", nextDate.getNextDate(28, 2, 2010));
     }
 }
